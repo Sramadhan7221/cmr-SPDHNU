4469640e4b0e5b2f35819036e2717268b36ded52
Syahrul Ramadhan <Sramadhan742@gmail.com>
2023-07-07 22:50:41 +0700

    modify db

---
+++ SPDHNU/app/Models/UserLembaga.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class UserLembaga extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_lembaga';
+    protected $primaryKey = 'id_user_lembaga';
+    protected $guarded = 'id_user_lembaga';
+    protected $fillable = [
+        'user_nik',
+        'id_lembaga'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+}
