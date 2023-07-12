3a72f0226542cb5405cbf2e581686e0315ea343e
Syahrul Ramadhan <Sramadhan742@gmail.com>
2023-07-12 09:06:06 +0700

    add edit and delete  persyaratan

--- SPDHNU/app/Http/Controllers/PersyaratanController.php
+++ SPDHNU/app/Http/Controllers/PersyaratanController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\RegisterUser;
 use App\Models\Persyaratan;
-use App\Models\Wilayah;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Session;
@@ -35,9 +33,6 @@ class PersyaratanController extends Controller
         $data = [
             'persyaratan' => $persyaratan,
             'user' => $user,
-            'kabupaten' => Wilayah::query()->where('kode', "32.06")->first(['kode', 'nama']),
-            'kecamatan' => Wilayah::query()->where('kode', $user->kecamatan)->first(['kode', 'nama']),
-            'desa' => Wilayah::getDesa($user->kecamatan),
             'display_menus' => $this->display_menus
         ];
         return view('SibahNU.persyaratan',$data);
@@ -68,8 +63,53 @@ class PersyaratanController extends Controller
         $file_name = $file->getClientOriginalName();
         $file_path = Storage::putFileAs($file,$file_name);
         $data['file'] = $file_path;
-        $data['id_lembaga'] = Session::get('id_lembaga');
+        $data['id_lembaga'] = session('id_lembaga');
         Persyaratan::create($data);
         return redirect(route('persyaratan'))->withSuccess('Data Berhasil Disimpan');
+    }
+
+    public function editPersyaratan(Request $request) {
+        $rules = [
+            'nama_persyaratan' => 'required',
+            'nomor_surat' => 'required',
+            'yang_mengeluarkan' => 'required',
+            // 'file' => 'required|max:2048'
+        ];
+
+        $message = [
+            'nama_persyaratan.required' => 'Nama surat harus diisi',
+            'nomor_surat.required' => 'Nomor surat harus diisi',
+            'yang_mengeluarkan.required' => 'Yang mengeluarkan harus diisi',
+            'file.required' => 'File harus diisi',
+            // 'file.max' => 'Ukuran file harus 2MB'
+        ];
+
+        $validated = Validator::make($request->all(), $rules, $message);
+        if ($validated->fails()) {
+            return redirect()->back()->withErrors($validated);
+        }
+
+        $data = $validated->validate();
+        $file = $request->file('file');
+        if ($file) {
+            $file_name = $file->getClientOriginalName();
+            $file_path = Storage::putFileAs($file, $file_name);
+            $data['file'] = $file_path;
+        }
+
+        Persyaratan::where('id_persyaratan',$request->id_persyaratan)
+            ->update($data);
+
+        return redirect(route('persyaratan'))->withSuccess('Data Berhasil Diupdate');
+    }
+
+    public function deletePersyaratan (Request $request) {
+        $deleted = Persyaratan::where('id_persyaratan', $request->id_persyaratan)
+            ->delete();
+        
+        if ($deleted > 0)
+            redirect(route('persyaratan'))->withSuccess('Data Berhasil Diupdate');
+        else
+            redirect(route('persyaratan'))->withErrors('Data Gagal Diupdate');
     }
 }
--- SPDHNU/resources/views/SibahNU/persyaratan.blade.php
+++ SPDHNU/resources/views/SibahNU/persyaratan.blade.php
@@ -3,182 +3,137 @@
 <div class="tab-pane fade show active" id="pills-data-persyaratan" role="tabpanel" aria-labelledby="data-persyaratan-tab">
   <!-- Table with stripped rows -->
   @include('sweetalert::alert')
- {{-- loading --}}
+  {{-- loading --}}
   <template x-if="isLoading">
     <div class="fixed inset-0 z-[100] bg-white">
-        <div class="flex h-screen w-full items-center justify-center bg-gray-100">
-          <div class="custom-loader"></div>
-        </div>
+      <div class="flex h-screen w-full items-center justify-center bg-gray-100">
+        <div class="custom-loader"></div>
       </div>
-    </template>
+    </div>
+  </template>
 
-    <div class="row mt-2 mb-3">
-        <div class="text-end">
-          <button
-            type="button"
-            class="btn btn-outline-success"
-            data-bs-toggle="modal"
-            data-bs-target="#verticalycentered">
-            <i class="ri-file-edit-line"></i>
-            Tambah Persyaratan Lembaga
-          </button>
-          <div
-            class="modal fade"
-            id="verticalycentered"
-            tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">
-                    Tambah Persyaratan Lembaga
-                  </h5>
-                  <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close">X</button>
-                </div>
-                <div class="modal-body">
-                  <form method="POST" action="{{route('addPersyaratan')}}" enctype="multipart/form-data">
-                    @csrf
-                    <div class="row g-3">
-                      <div class="col-md-12">
-                        <label
-                          for="input-jenis-dokumen"
-                          class="form-label d-flex justify-content-start">
-                          Jenis Dokumen
-                          <sup class="text-danger">*</sup>
-                        </label>
-                        <select
-                          id="input-jenis-dokumen"
-                          class="form-select">
-                          <option selected>Choose...</option>
-                          <option>...</option>
-                        </select>
-                      </div>
-
-                      <div class="row g-3">
-                        <div class="col-md-12">
-                          <label
-                            for="input-nama-surat"
-                            class="form-label d-flex justify-content-start">
-                            Nama Surat
-                            <sup class="text-danger">*</sup>
-                          </label>
-                          <input
-                            type="text"
-                            name="nama_persyaratan"
-                            class="form-control"
-                            id="input-nama-surat"
-                            required />
-                        </div>
-                      </div>
-
-                      <div class="row g-3">
-                        <div class="col-md-12">
-                          <label
-                            for="input-no-surat"
-                            class="form-label d-flex justify-content-start">
-                            Nomor Surat
-                            <sup class="text-danger">*</sup>
-                          </label>
-                          <input
-                            type="text"
-                            name="nomor_surat"
-                            class="form-control"
-                            id="input-no-surat"
-                            required />
-                        </div>
-                      </div>
-
-                      <div class="row g-3">
-                        <div class="col-md-12">
-                          <label
-                            for="input-keluarkan"
-                            class="form-label d-flex justify-content-start">
-                            Mengeluarkan
-                            <sup class="text-danger">*</sup>
-                          </label>
-                          <input
-                            type="text"
-                            name="yang_mengeluarkan"
-                            class="form-control"
-                            id="input-keluarkan"
-                            required />
-                        </div>
-                      </div>
-
-                      <div class="row g-3">
-                        <div class="col-md-12">
-                          <label
-                            for="input-file-surat"
-                            class="form-label d-flex justify-content-start">
-                            File Surat
-                            <sup class="text-danger">*</sup>
-                          </label>
-                          <input
-                            type="file"
-                            class="form-control"
-                            name="file"
-                            id="input-file-surat"
-                            required />
-                          <span
-                            class="badge bg-success d-flex justify-content-start">
-                            File harus berupa PDF
-                          </span>
-                        </div>
-                      </div>
+  <div class="row mt-2 mb-3">
+    <div class="text-end">
+      <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#verticalycentered">
+        <i class="ri-file-edit-line"></i>
+        Tambah Persyaratan Lembaga
+      </button>
+      <div class="modal fade" id="verticalycentered" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">
+                Tambah Persyaratan Lembaga
+              </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+            </div>
+            <div class="modal-body">
+              <form id="persyaratan_form" method="POST" action="{{route('addPersyaratan')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="row g-3">
+                  <input type="hidden" name="id_persyaratan"/>
+                  <div class="row g-3">
+                    <div class="col-md-12">
+                      <label for="input-nama-surat" class="form-label d-flex justify-content-start">
+                        Nama Surat
+                        <sup class="text-danger">*</sup>
+                      </label>
+                      <input type="text" name="nama_persyaratan" class="form-control" id="input-nama-surat" required />
                     </div>
-                    <div class="modal-footer">
-                      <button
-                        type="submit"
-                        class="btn btn-success">
-                        <i class="ri-file-edit-line"></i>
-                        Simpan data
-                      </button>
-                  </form>
+                  </div>
+
+                  <div class="row g-3">
+                    <div class="col-md-12">
+                      <label for="input-no-surat" class="form-label d-flex justify-content-start">
+                        Nomor Surat
+                        <sup class="text-danger">*</sup>
+                      </label>
+                      <input type="text" name="nomor_surat" class="form-control" id="input-no-surat" required />
+                    </div>
+                  </div>
+
+                  <div class="row g-3">
+                    <div class="col-md-12">
+                      <label for="input-keluarkan" class="form-label d-flex justify-content-start">
+                        Mengeluarkan
+                        <sup class="text-danger">*</sup>
+                      </label>
+                      <input type="text" name="yang_mengeluarkan" class="form-control" id="input-keluarkan" required />
+                    </div>
+                  </div>
+
+                  <div class="row g-3">
+                    <div class="col-md-12">
+                      <label for="input-file-surat" class="form-label d-flex justify-content-start">
+                        File Surat
+                        <sup class="text-danger">*</sup>
+                      </label>
+                      <input type="file" class="form-control" name="file" id="input-file-surat" required />
+                      <span class="badge bg-success d-flex justify-content-start">
+                        File harus berupa PDF
+                      </span>
+                    </div>
+                  </div>
                 </div>
-                </div>
-              </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-success">
+                    <i class="ri-file-edit-line"></i>
+                    Simpan data
+                  </button>
+              </form>
             </div>
           </div>
-          <!-- End Vertically centered Modal-->
         </div>
       </div>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Nama Persyaratan</th>
-                <th scope="col">Nomor Surat</th>
-                <th scope="col">Yang Mengeluarkan</th>
-                <th scope="col">File</th>
-                <th scope="col">Aksi</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($persyaratan as $syarat)
-            <tr>
-                <th scope="row">1</th>
-                <td>{{$syarat->nama_persyaratan}}</td>
-                <td>{{$syarat->nomor_surat}}</td>
-                <td>{{$syarat->yang_mengeluarkan}}</td>
-                <td><a href="{{asset('storage/'.$syarat->file)}}" class="bg-green-800 p-2 rounded-md text-white">Lihat File</a></td>
-                <td></td>
-            </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <!-- End Table with stripped rows -->
+    </div>
+    <!-- End Vertically centered Modal-->
+  </div>
+</div>
 
-        <div class="row g-3 pt-4 mt-4 mb-4">
-            <div class="text-end">
-            <button type="submit" class="bg-green-800 p-2 rounded-md text-white col-md-3">
-                <i class="ri-file-edit-line"></i>
-                Perbaharui data MWCNU
-            </button>
-            </div>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama Persyaratan</th>
+      <th scope="col">Nomor Surat</th>
+      <th scope="col">Yang Mengeluarkan</th>
+      <th scope="col">File</th>
+      <th scope="col">Aksi</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($persyaratan as $syarat)
+    <tr>
+      <th scope="row">1</th>
+      <td>{{$syarat->nama_persyaratan}}</td>
+      <td>{{$syarat->nomor_surat}}</td>
+      <td>{{$syarat->yang_mengeluarkan}}</td>
+      <td><a href="{{asset('storage/'.$syarat->file)}}" class="bg-green-800 p-2 rounded-md text-white">Lihat File</a></td>
+      <td>
+        <div class="row">
+          <div class="col-sm-12">
+            <button type="button" class="edit col-sm-5 bg-yellow-400 p-1 rounded-md" data-syarat="{{$syarat->id_persyaratan}}"><x-heroicon-o-pencil-square /></button>
+            <button type="button" class="delete col-sm-5 bg-red-600 p-1 rounded-md" data-syarat="{{$syarat->id_persyaratan}}"><x-heroicon-o-backspace /></button>
+          </div>
         </div>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+<!-- End Table with stripped rows -->
+
+<div class="row g-3 pt-4 mt-4 mb-4">
+  <div class="text-end">
+    <button type="submit" class="bg-green-800 p-2 rounded-md text-white col-md-3">
+      <i class="ri-file-edit-line"></i>
+      Perbaharui data MWCNU
+    </button>
+  </div>
+</div>
 </div>
 @include('SibahNU.template.footer')
+<script>
+
+</script>
